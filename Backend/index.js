1b72670b011c6ea8a230e60770750521c71301e6
rishidutta5 <rishirajdutta1505@gmail.com>
2026-08-14 18:27:36 +0530

    Update MongoDB connection and server port handling

--- Backend/index.js
+++ Backend/index.js
@@ -12,7 +12,7 @@ const todoRouter = require("./routes/todo.routes");
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb+srv://rishirajdutta1505_db_user:n9yn4kQw0e89AE7y@cluster0.9l8bq8w.mongodb.net/?appName=Cluster0")
+mongoose.connect(process.env.URL)
 .then(()=>{
     console.log("MongoDB connected")
 })
@@ -23,6 +23,7 @@ mongoose.connect("mongodb+srv://rishirajdutta1505_db_user:n9yn4kQw0e89AE7y@clust
 app.use("/api/auth", userRouter);
 app.use("/api/todos", todoRouter);
 
-app.listen(3000, ()=>{
-    console.log(`server running on port 3000`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, ()=>{
+    console.log(`Server running on port ${PORT}`);
 })
